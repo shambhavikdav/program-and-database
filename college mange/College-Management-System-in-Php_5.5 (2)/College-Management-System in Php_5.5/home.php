da3b962212401f13a11de6613c7013e801242c0d
shambhavikdav <59994855+shambhavikdav@users.noreply.github.com>
2020-03-24 17:48:04 +0530

    Add files via upload

---
+++ college mange/College-Management-System-in-Php_5.5 (2)/College-Management-System in Php_5.5/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>College management system made by Shambhavi Sayali Pooja..</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/rtupic.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>A. P. Shah Institute of Technology (APSIT) has started functioning with commitment of imparting state of art technical education so as to inculcate conceptual know-how, analyzing skills, decision making abilities and leadership qualities in the students. APSIT stands committed to the intellectual and moral growth of every student.
+
+   APSIT has experienced and proficient team which aspires to unlock the hidden potential in subconscious minds of students and to create competent Engineers with vision & social commitment.</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>Vision:
+ 
+APSIT aspires to be a premier institute producing globally competent engineering professionals to contribute towards socio-economic growth of India.</p>         
+        </div>   
+               
+
+
+
+
+
+
+
+ </div>
+        </div>
+    
+    
+</body>
+</html>
